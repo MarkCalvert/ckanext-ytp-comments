e9cbaf79883bdc972c5cbbcea4a6e72cb6c6a15b
Mark Calvert <mark.calvert@salsadigital.com.au>
2020-06-27 12:46:10 -0600

    [DQL2-38] - Highlight a list of comments from the comment_ids parameter

--- ckanext/ytp/comments/public/javascript/comments.js
+++ ckanext/ytp/comments/public/javascript/comments.js
@@ -1,13 +1,25 @@
-function ShowCommentForm(id){
+function ShowCommentForm(id) {
     $("#" + id).removeClass('hidden');
 }
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
-    jQuery('.comment-container form .btn-cancel').on('click', function(e) {
+    jQuery('.comment-container form .btn-cancel').on('click', function (e) {
         var form = jQuery(this).closest('form');
         if (form) {
             form.addClass('hidden');
         }
     });
+
+    if (window.location.search) {
+        let params = new URLSearchParams(window.location.search);
+        // If parameter exists, highlight the comment sections from the list of comment_ids provided
+        if (params.has('comment_ids')) {
+            comment_ids = params.get('comment_ids')
+            comment_ids.split(',').forEach(id => {
+                jQuery('#comment_' + id).addClass('highlight');
+            });
+        }
+    }
+
 });
